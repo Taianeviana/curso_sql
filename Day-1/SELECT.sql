fc6a703a2c1f508576afc6506953c12199dc7a11
taianevcarvalho@gmail.com <taianevcarvalho@gmail.com>
2022-12-14 01:02:27 +0000

    primeiro dia

--- Day-1/SELECT.sql
+++ Day-1/SELECT.sql
@@ -6,3 +6,40 @@ SELECT * FROM silver_olist.pedido LIMIT 5
 SELECT *,
         DATEDIFF(dtEstimativaEntrega, dtEntregue) as qtdDiasPromessaEntrega
 FROM silver_olist.pedido
+
+-- COMMAND ----------
+
+--WHERE
+
+
+-- COMMAND ----------
+
+SELECT * FROM silver_olist.pedido
+WHERE descSituacao = 'canceled'
+
+-- COMMAND ----------
+
+SELECT * FROM silver_olist.pedido
+WHERE descSituacao = 'shipped'
+AND year(dtPedido) = '2018'
+
+-- COMMAND ----------
+
+SELECT * FROM silver_olist.pedido
+WHERE (descSituacao = 'shipped'
+OR descSituacao = 'canceled')
+AND year(dtPedido) = '2018'
+
+
+-- COMMAND ----------
+
+SELECT * FROM silver_olist.pedido
+WHERE descSituacao IN ('shipped', 'canceled')
+AND year(dtPedido) = '2018'
+
+-- COMMAND ----------
+
+SELECT * FROM silver_olist.pedido
+WHERE descSituacao IN ('shipped', 'canceled')
+AND year(dtPedido) = '2018'
+AND DATEDIFF(dtEstimativaEntrega, dtAprovado) > 30
